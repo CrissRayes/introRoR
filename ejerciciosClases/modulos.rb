8e23d96bf6726b612efff74132a0fb3b0ab9dd3b
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-05-05 17:45:57 -0400

    actualizando modulos

--- ejerciciosClases/modulos.rb
+++ ejerciciosClases/modulos.rb
@@ -13,12 +13,10 @@
 ## Podemos emular el comportamiento de la clase Sale con un módulo
 ## Y en menos líneas de código
 module Sale
-  module FormBuilder # Ahora, agregamos un submodulo para ver como se comporta
     def self.call(params)
       puts "Params in module: #{params}" # Ojo, es variable local, no es variable de instancia
     end
-  end
 end
 
-Sale::FormBuilder.call("Some data")
+Sale.call("Some data")
 
